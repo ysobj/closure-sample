68c1da422786f2458955437463d9ebd44bce508b
kougo <kougo@techmatrix.co.jp>
2013-02-15 18:51:38 +0900

    とりあえず...

--- scripts/com/karatebancho/apps.js
+++ scripts/com/karatebancho/apps.js
@@ -2,6 +2,9 @@ goog.provide('com.karatebancho.Apps');
 goog.require('com.karatebancho.Kgrid');
 
 com.karatebancho.Apps = function(){
-		var grid = new com.karatebancho.Kgrid();
-		grid.render();
+  var grid = new com.karatebancho.Kgrid([
+      {label: '顧客番号', name: 'abc'},
+      {label: '顧客姓', name: 'def'},
+      {label: '顧客名', name: 'ghi'}]);
+  grid.render();
 };
--- scripts/com/karatebancho/kgrid.js
+++ scripts/com/karatebancho/kgrid.js
@@ -1,16 +1,77 @@
 goog.provide('com.karatebancho.Kgrid');
 
+goog.require('goog.array');
+goog.require('goog.object');
+goog.require('goog.style');
 goog.require('goog.ui.Component');
-goog.require('goog.dom');
+
+
+
 /**
+ *
+ * グリッドコントロールのコンポーネントです
+ *
+ * @extends {goog.ui.Component}
  * @constructor
+ * @param {Array} headers ヘッダ.
+ * @param {Object=} opt_contents コンテンツ.
+ * @param {Object=} opt_options オプション.
+ * @param {goog.dom.DomHelper=} opt_domHelper Optional DOM helper.
  */
-com.karatebancho.Kgrid = function(){
-		goog.base(this);
+com.karatebancho.Kgrid = function(
+  headers, opt_contents, opt_options, opt_domHelper) {
+  goog.ui.Component.call(this, opt_domHelper);
+  this.headers_ = headers;
+  this.contents_ = opt_contents || [];
+  this.options_ = opt_options || [];
 };
 goog.inherits(com.karatebancho.Kgrid, goog.ui.Component);
 
-com.karatebancho.Kgrid.prototype.createDom = function(){
-		alert('hoge');
-		this.setElementInternal(goog.dom.htmlToDocumentFragment("<table></table>"));
+
+/**
+ * @type {string}
+ * @private
+ */
+com.karatebancho.Kgrid.CLASS_NAME_ = goog.getCssName('kgrid');
+
+
+/**
+ * @override
+ */
+com.karatebancho.Kgrid.prototype.createDom = function() {
+  var dh = this.getDomHelper()
+  this.tbl_ = dh.createDom('table'), hds = [];
+  goog.array.forEach(this.headers_, function(el, ind, arr) {
+      hds.push(dh.createDom('th', undefined, el.label));
+    }, this);
+  this.tbl_.appendChild(dh.createDom('tr', undefined, hds));
+
+  this.decorateInternal(this.tbl_);
 };
+
+
+/**
+ * @override
+ */
+com.karatebancho.Kgrid.prototype.enterDocument = function() {
+  goog.base(this, 'enterDocument');
+};
+
+
+/**
+ *  @param {Array} rows 設定する行.
+ */
+com.karatebancho.Kgrid.prototype.setRows = function(rows) {
+};
+
+/**
+ * @param {Object} row 追加する行.
+ */
+com.karatebancho.Kgrid.prototype.addRow = function(row) {
+  var dh = this.getDomHelper();
+  var values = [];
+  goog.array.forEach(this.headers_, function(el, ind, arr) {
+      values.push(dh.createDom('td', undefined, row[el.name]));
+    },this);
+};
+
